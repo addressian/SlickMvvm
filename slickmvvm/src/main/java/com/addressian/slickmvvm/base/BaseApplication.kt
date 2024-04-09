386b6d43aff4b45fd53acdc79171e6bab3c8c90e
addressian <33949578+addressian@users.noreply.github.com>
2024-04-09 18:10:19 +0800

    Update BaseApplication.kt

--- slickmvvm/src/main/java/com/addressian/slickmvvm/base/BaseApplication.kt
+++ slickmvvm/src/main/java/com/addressian/slickmvvm/base/BaseApplication.kt
@@ -4,17 +4,12 @@ import android.app.Application
 import com.addressian.slickmvvm.utils.AppearanceUtils
 import com.addressian.slickmvvm.utils.MMKVOwner
 
-class BaseApplication : Application() {
+open class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        instance = this
         MMKVOwner.initialize(this)
         AppearanceUtils.initNightMode()
     }
 
-    companion object {
-        private lateinit var instance: BaseApplication
-        fun getInstance() = instance
-    }
 }
