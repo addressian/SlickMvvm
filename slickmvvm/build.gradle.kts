9c77928d6cd6596fc8294c3ec6056e6dd9f36bba
addressian <33949578+addressian@users.noreply.github.com>
2024-04-09 11:35:46 +0800

    remove unused resource

--- slickmvvm/build.gradle.kts
+++ slickmvvm/build.gradle.kts
@@ -10,7 +10,7 @@ publishing {
         register<MavenPublication>("MyLibrary") {
             groupId = "com.addressian.slickmvvm"
             artifactId = "slickMvvm"
-            version = "0.0.05"
+            version = "0.0.06"
 
             afterEvaluate {
                 from(components["release"])
